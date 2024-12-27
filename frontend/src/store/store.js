dc39844a80e161c59ebf8debb487ce141b951690
Vijay N <nvijay115@gmail.com>
2024-12-27 13:50:10 +0530

    Wishlist Functionality Update

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer  from "../slice/cartSlice";
+import wishlistReducer from "../slice/wishlistSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    wishlist: wishlistReducer,
   },
 });
 
