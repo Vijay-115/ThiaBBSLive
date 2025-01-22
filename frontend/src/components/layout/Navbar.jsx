64f48c08b60d52eddaa931c70016e99e0d9c6c34
Ganapathy <ganapathy569164@gmail.com>
2025-01-22 15:32:01 +0530

    Responsive Update

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -3,7 +3,7 @@ import NavItems from "./NavItems"
 function Navbar({menuOpen,closeMenu}) {
   return (
     <>
-        <nav className='navbar overflow-hidden'>
+        <nav className='navbar'>
             <div className='md:bg-slate-100 w-full py-2 px-4'>
                 <NavItems menuOpen={menuOpen} closeMenu={closeMenu}/>
             </div>
