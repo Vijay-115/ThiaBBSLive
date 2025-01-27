fa1a87b1f2dcd5cd84255ac9a442166193506857
Ganapathy <ganapathy569164@gmail.com>
2025-01-27 13:40:47 +0530

    Product CURD

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const { ObjectId } = mongoose.Schema.Types;
 
 const OrderSchema = new mongoose.Schema({
-  order_id: { type: ObjectId, required: true, unique: true }, // Unique identifier for the order
+  order_id: { type: String, required: true, unique: true }, // Unique identifier for the order
   user_id: ObjectId, // ID of the user placing the order (reference to Users collection)
   products: [
     {
