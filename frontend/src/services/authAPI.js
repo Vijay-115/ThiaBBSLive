d4caaf61768757db9140b860d0ef7e9b489aba95
bbspon <bbspon@gmail.com>
2025-08-06 11:25:07 +0530

    update uri

--- frontend/src/services/authAPI.js
+++ frontend/src/services/authAPI.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-console.log("BASE URL:", import.meta.env.VITE_API_URI);  // :check: This will now log the value
+console.log("BASE URL:", import.meta.env.VITE_API_URL);  // :check: This will now log the value
 
 const API = axios.create({
 
-baseURL: `${import.meta.env.VITE_API_URI}/auth`,
+baseURL: `${import.meta.env.VITE_API_URL}/auth`,
 
 });
 
